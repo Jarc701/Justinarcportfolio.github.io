4eff11ddffd8d6070dca44a54a024e050d3ff9b4
JUSTIN ARCHER <121035784+Jarc701@users.noreply.github.com>
2023-01-06 13:03:50 +0000

    I added the other pages part of the portfolio by creating a div for all pages

---
+++ script.js
@@ -0,0 +1,14 @@
+$(".About").click(function(){
+$(".Home_Page").hide();
+$(".About_Me_Page").show();
+});
+
+$(".Contact").click(function(){
+$(".Home_Page").hide();
+$(".Contact_Page").show();
+});
+
+$(".Projects").click(function(){
+$(".Home_Page").hide();
+$(".Projects_Page").show();
+});
