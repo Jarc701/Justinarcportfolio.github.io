cf3d56e73eef2aedfc4a4167cf7a65cf59ea05d9
JUSTIN ARCHER <121035784+Jarc701@users.noreply.github.com>
2023-01-06 14:33:10 +0000

    I added more parts to each page

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ $(".Home_Page").hide();
 $(".Contact_Page").show();
 });
 
+$(".Resume").click(function(){
+$(".Home_Page").hide();
+$(".Resume_Page").show();
+});
+
 $(".Projects").click(function(){
 $(".Home_Page").hide();
 $(".Projects_Page").show();
